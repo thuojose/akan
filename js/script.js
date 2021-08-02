6828c4250329c1a94e7900230cd036a30f24f0d4
joseph mbugua <josephmbugus16@gmail.com>
2021-08-02 08:06:36 +0300

    fixing indentation

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let totalamount = document.querySelector('.price-bar')
 let delivery = document.querySelector('.order-bar')
-let price = document.querySelector('.order-bar')
+let ordernow = document.querySelector('.order-bar')
 let value = document.querySelector('.ordernow-bar')
 menu.onclick = () =>{
 
@@ -40,74 +40,4 @@ function getTotalAmount() {
       return total.value;
     }
   }
-}
-// The order is stored here
-var order = [];
-var max_pizzas = large;
-var orders = load_orders();
-var delivery_price = 3.00;
-
-var pizzas = [
-  {name: 'Supermisso', price: 8.50, image: 'pizza-1.png'},
-  {name: 'Supermisso', price: 8.50, image: 'pizza-2.png'},
-  {name: 'Supermisso', price: 8.50, image: 'pizza-3.png'},
-  {name: 'Supermisso', price: 8.50, image: 'pizza-4.png'},
-  {name: 'Supermisso', price: 8.50, image: 'pizza-1.png'},
-  {name: 'Supermisso', price: 8.50, image: 'pizza-2.png'},
-  {name: 'Supermisso', price: 8.50, image: 'pizza-3.png'},
-  {name: 'Supermisso', price: 13.50, image: 'pizza-4.png'},
-  {name: 'Supermisso', price: 13.50, image: 'pizza-1.png'},
-  {name: 'Supermisso', price: 13.50, image: 'pizza-2.png'},
-  {name: 'Supermisso', price: 13.50, image: 'pizza-3.png'},
-  {name: 'Supermisso', price: 13.50, image: 'pizza-4.png'}
-];
-
-
-// Try and load orders from localStorage
-function load_orders(){
-  try {
-    return JSON.parse(window.localStorage["orders"]);
-  }
-  catch (e) {
-    return [];
-  }
-}
-
-function save_order() {
-  orders.push({pizzas: order, form: $('form').serializeArray()});
-
-  save_orders();
-}
-
-function save_orders() {
-  localStorage.setItem('orders', JSON.stringify(orders));
-}
-
-function get_index_html(pizzas, delivery, show_delete) {
-    order_html = "";
-
-    total = 0.0;
-
-    // Add Pizzas to the order
-    for (var i = 0; i < pizzas.length; ++i) {
-      pizza = pizzas[i];
-      total += pizza.price;
-
-      order_html += '<tr id="' + i + '"><th>' + pizza.name + '</th><td>$' + pizza.price.toFixed(2) + '</td>';
-      if (show_delete) {
-        index_html += '<td><button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-remove"></span></button></td>';
-      }
-      index_html += '</tr>';
-    }
-
-    // Add Delivery to the order
-    if (delivery) {
-      total += delivery_price;
-      index_html += '<tr><th>Delivery</th><td>$' + delivery_price.toFixed(2) + '</td></tr>';
-    }
-
-    // Add totalamount
-    index_html += '<tr><th>Total</th><th>$' + total + '</th></tr>';
-
-    return index_html;
 }
